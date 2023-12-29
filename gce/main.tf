28e0a791d7c8912ba03efcd97cd9a5074da775b6
sivaram sajeev <sivaramsajeev@sivarams-MacBook-Air.local>
2023-12-29 17:49:26 +0000

    added dns project

--- gce/main.tf
+++ gce/main.tf
@@ -32,12 +32,11 @@ resource "google_compute_instance" "gce" {
 
 resource "google_dns_record_set" "gce_dns" {
   count = var.external_ip_enabled == true ? 1:0
-#   name = "frontend.${google_dns_managed_zone.prod.dns_name}"
+
+  project      = var.project_name
   name = "${var.instance_name}.${var.domain_name}."
   type = "A"
   ttl  = 300
-
   managed_zone = replace(var.domain_name, ".", "-")
-
   rrdatas = [ google_compute_instance.gce.network_interface[0].access_config[0].nat_ip ]
 }
