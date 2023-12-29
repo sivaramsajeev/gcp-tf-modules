8f1413801ead83b626d76e9c7af467d1c7695d90
sivaram sajeev <sivaramsajeev@sivarams-MacBook-Air.local>
2023-12-29 17:58:53 +0000

    mod dns name

--- dns/main.tf
+++ dns/main.tf
@@ -1,6 +1,6 @@
 resource "google_dns_managed_zone" "public-zone" {
   project     = var.project_name
   name        = replace(var.domain_name, ".", "-")
-  dns_name    = var.domain_name
+  dns_name    = "${var.domain_name}."
   description = "Public DNS zone for ${var.domain_name}"
 }
