deb3ac063d712de3686aa2f8a94377b618446431
sivaram sajeev <sivaramsajeev@sivarams-MacBook-Air.local>
2023-12-30 02:00:56 +0000

    firewall rules mod

--- firewall/variables.tf
+++ firewall/variables.tf
@@ -10,8 +10,24 @@ variable project_name {
     type = string 
 }
 
-variable rules {}
 
 variable source_ranges {
     type = list(string)
+}
+
+variable "rules" {
+  type = map(object({
+    protocol = string
+    ports    = list(number)
+  }))
+  default = {
+    rule1 = {
+      protocol = "tcp"
+      ports    = [80, 443]
+    }
+    rule2 = {
+      protocol = "udp"
+      ports    = [53]
+    }
+  }
 }
