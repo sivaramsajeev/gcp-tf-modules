bd6de4ad17e18dacc8e500c4a9945cf2cc1e32fb
sivaram sajeev <sivaramsajeev@sivarams-MacBook-Air.local>
2023-12-30 01:50:31 +0000

    firewall added

---
+++ firewall/main.tf
@@ -0,0 +1,15 @@
+resource "google_compute_firewall" "firewall_allow" {
+  name      = var.firewall_name
+  network   = var.network
+  project   = var.project_name
+
+  dynamic "allow" {
+    for_each = var.rules
+    content {
+      protocol = each.value.protocol
+      ports    = each.value.ports
+    }
+  }
+
+  source_ranges = var.source_ranges
+}
---
+++ firewall/variables.tf
@@ -0,0 +1,17 @@
+variable firewall_name {
+    type = string 
+}
+
+variable network {
+    type = string 
+}
+
+variable project_name {
+    type = string 
+}
+
+variable rules {}
+
+variable source_ranges {
+    type = list(string)
+}
